e1dce17dd16dbf0e5e06e3f58c1291458ff8a483
SarahBestest <70079779+SarahBestest@users.noreply.github.com>
2020-08-23 22:38:21 +0300

    Update skills.js

--- data/eox/skills.js
+++ data/eox/skills.js
@@ -9,6 +9,16 @@ let skills = {
       dep: { },
       maxLevel: 10,
       coords: { x: 0, y: 0 }
+    },
+    triumphantCry: {
+      name_en: "Triumphant Cry",
+      desc: "Restore party's HP if user is alive at the end of battle.",
+      stats: [],
+      unique: false,
+      type: "Passive",
+      dep: { },
+      maxLevel: 10,
+      coords: { x: 1, y: 0 }
     }
   }
 };
